7c0ffe27333df242c7f67bf83aeeb5be62f2cfce
mingyangli <mingyangli@emotibot.com>
2024-04-10 20:21:32 +0800

    download number

--- app/live/page.jsx
+++ app/live/page.jsx
@@ -1,9 +1,34 @@
 "use client";
-import { Avatar, Image } from "@nextui-org/react"
+import { Avatar, Image, User } from "@nextui-org/react"
 import { useEffect, useState, useRef } from 'react';
 import { Transition } from '@headlessui/react'
 import users from "./users.json"
+import { getAppData } from "@/requests/appData"
+
 export default function LivePage() {
+  // 取随机数
+  function randomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  function getChatTimeRandom() {
+    return randomIntInclusive(appData.ext.comment_inverval.min, appData.ext.comment_inverval.max)
+  }
+  function getPopupTimeRandom() {
+    return randomIntInclusive(appData.ext.pop_inverval.min, appData.ext.pop_inverval.max)
+  }
+  function getRandomComment() {
+    const randomIndex = randomIntInclusive(0, appData.ext.comments.length - 1)
+    return appData.ext.comments[randomIndex]
+  }
+  const [appData, setAppData] = useState(null)
+  const [roomUsersCount, setRoomUsersCount] = useState(0)
+  useEffect(() => {
+    getAppData().then((appDataSync) => {
+      console.error("appDataSync", appDataSync)
+      setAppData(appDataSync)
+      setRoomUsersCount(appDataSync.ext.roomUsersCount)
+    })
+  }, [])
   // video muted 切换
   const video = useRef(null)
   useEffect(() => {
@@ -19,44 +44,76 @@ export default function LivePage() {
   const [enterList, setEnterList] = useState([])
   const [currentEnterUser, setCurrentEnterUser] = useState({})
   useEffect(() => {
-    const interval = setInterval(() => {
-      setEnterList((enterList) => [...enterList, {
-        name: 'H***'
-      }])
-      console.error("enterlist", enterList)
-    },3000)
-    return () => {
-      clearInterval(interval)
+    if(appData) {
+      pushPopUpUser()
     }
-  },[])
+  },[appData])
+  function pushPopUpUser() {
+    const randomTime = getPopupTimeRandom()
+    setTimeout(() => {
+      const randomUser = users[randomIntInclusive(0, users.length - 1)]
+      setEnterList((enterList) => [...enterList, {
+        avatarUrl: randomUser?.avatarUrl,
+        name: randomUser?.nickname || 'H***'
+      }])
+      setRoomUsersCount( userCount => userCount += 1)
+      pushPopUpUser()
+    }, randomTime);
+  }
   useEffect(() => {
-    const item = enterList.shift()
-    setCurrentEnterUser(item)
-    showPopup()
+    if(enterList.length) {
+      const item = enterList.shift()
+      setCurrentEnterUser(item)
+      showPopup()
+    }
   },[enterList])
 
   function showPopup() {
     setIsShowing(true)
     setTimeout(() => {
       setIsShowing(false)
-    }, 2000)
+    }, 3000)
   }
   // 处理chat
-  const fullUsers = useRef(users)
+  const fullUsers = useRef(JSON.parse(JSON.stringify(users)))
+  let userIndex = 10
   const [chatList, setChatList] = useState([])
   useEffect(() => {
-    setChatList(fullUsers.current.splice(0, 10))
-    const interval = setInterval(() => {
-      const user = fullUsers.current.shift()
+    if(appData) {
+      setChatList(fullUsers.current.slice(0, 10).map(item => {
+        item.msg = getRandomComment()
+        return item
+      }))
+      pushUserComment()
+    }
+  },[appData])
+  function pushUserComment() {
+    const randomTime = getChatTimeRandom()
+    setTimeout(() => {
+      const user = fullUsers.current[userIndex % users.length]
+      userIndex ++
+      // const randomComments = appData.ext.comments
       setChatList((chatList) => [...chatList, {
         avatarUrl: user.avatarUrl,
-        msg: 'Hello Hello'
+        msg: getRandomComment()
       }])
-    }, 2000)
-    return () => {
-      clearInterval(interval)
+      pushUserComment()
+    }, randomTime);
+  }
+  const [userInput, setUserInput] = useState('')
+  function onInputConfirm(event) {
+    if (event.key === "Enter") {
+      userUserComment(userInput)
+      setUserInput("")
     }
-  },[])
+  }
+  function userUserComment(msg) {
+    const avatarUrl = appData.ext.commentUser.avatarUrl
+    setChatList((chatList) => [...chatList, {
+      avatarUrl,
+      msg: msg
+    }])
+  }
   useEffect(() => {
     // gundong
     smoothScrollToBottom()
@@ -78,7 +135,7 @@ export default function LivePage() {
     setTimeout(() => {
       setshowDownload(true)
       setTimeout(() => setShowDwnloadNumber(true), 100)
-    }, 1000);
+    }, 2000);
   },[])
 
   // 监听窗口大小 处理浏览器兼容
@@ -96,25 +153,30 @@ export default function LivePage() {
     }
   }, [])
 
-  
-  useEffect(() => {
-  }, [])
+  // 下载apk
+  function downloadApk() {
+    window.open(appData.ext.download, "_blank")
+  }
 
   return (
     <div ref={page} className="h-[100vh] flex flex-col bg-[url('/bg.png')]">
       <div className="h-[50px] flex justify-between items-center px-[10px]">
         <div className="flex h-[40px] items-center rounded-full pl-[2px] pr-[20px]">
-          <Avatar className="w-[36px] h-[36px]" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+          <Avatar className="w-[36px] h-[36px]" src={appData?.ext.liveRoomInfo.avatarUrl} />
           <div className="text-white flex flex-col ml-[6px]">
-            <span className="font-bold text-[12px] leading-[16px]">LinKa</span>
-            <span className="text-[10px] leading-[14px]">100 Follow</span>
+            <span className="font-bold text-[12px] leading-[16px]">{appData?.ext.liveRoomInfo.nickname}</span>
+            <span className="text-[10px] leading-[14px]">{appData?.ext.liveRoomInfo.follows} Follow</span>
           </div>
         </div>
         <div className="flex items-center gap-1">
-          <Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
-          <Avatar size="sm" src="https://i.pravatar.cc/150?u=a042581f4e29026704d" />
-          <Avatar size="sm"src="https://i.pravatar.cc/150?u=a04258114e29026702d" />
-          <Avatar size="sm" className="text-white bg-[#333] opacity-60" name="80" />
+          {
+            appData?.ext.roomUsers && appData?.ext.roomUsers.map((item, index) => {
+              return (
+                <Avatar size="sm" src={ item.avatarUrl } key={index}/>
+              )
+            })
+          }
+          <Avatar size="sm" className="text-white bg-[#333] opacity-60" name={ String(roomUsersCount) } />
         </div>
       </div>
       <div className="relative">
@@ -150,7 +212,7 @@ export default function LivePage() {
           >
             <div className="bg-gradient-to-r from-[rgba(120,120,255,1)] to-[rgba(83,147,227,0.7)]
               max-w-[350px] h-[45px] rounded-full flex items-center pl-[6px] pr-[20px] text-[14px]">
-              <Avatar className="w-[36px] h-[36px]" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+              <Avatar className="w-[36px] h-[36px]" src={currentEnterUser?.avatarUrl} />
               <div className="ml-2 text-white">
                 <span className="text-[#f0d67f]">{ currentEnterUser?.name }</span>
                 <span className="ml-2">Enter the live broadcast room</span>
@@ -163,8 +225,8 @@ export default function LivePage() {
             chatList.map((item, index) => {
               return (
                 <div key={index} className="mb-[10px] flex">
-                  <Avatar src={ item.avatarUrl } className="w-[34px] h-[34px]"/>
-                  <div className="bg-[#2e3e4e] flex-1 break-all text-white ml-2 p-2 rounded-lg">
+                  <Avatar src={ item.avatarUrl } className="w-[34px] h-[34px] min-w-[34px]"/>
+                  <div className="bg-[#2e3e4e] break-word text-white ml-2 p-2 rounded-lg w-[fit-content]">
                     { item.msg }
                   </div>
                 </div>
@@ -173,7 +235,7 @@ export default function LivePage() {
           }
         </div>
         <div className="flex-1 h-[80px] px-[20px] flex items-center">
-          <input type="text" placeholder="Say Somethings ..." className="outline-none px-[15px]
+          <input value={userInput} onInput={e => setUserInput(e.target.value)} onKeyDown={ onInputConfirm } placeholder="Say Somethings ..." className="outline-none px-[15px]
             w-full bg-[#201c1b] h-[50px] rounded-full text-[16px] text-white"/>
         </div>
         <div className="bottom-[80px] absolute right-[15px]">
@@ -181,7 +243,7 @@ export default function LivePage() {
             showDwnloadNumber && <div className="bg-gradient-to-r from-[rgba(233,149,59,1)] to-[rgba(233,149,59,0)]
             rounded-full h-[40px] text-[#fff] flex items-center justify-center text-[13px] mb-[10px]">
               <Avatar src="/fire.svg" className="bg-[transparent] w-[23px] h-[23px]"/>
-              <span className="ml-[2px]">567 Download</span>
+              <span className="ml-[2px]">{ appData?.ext.downloadNumber || 566 } Download</span>
             </div>
           }
           <Transition
@@ -198,13 +260,10 @@ export default function LivePage() {
               <Avatar className='w-[80px] h-[80px]' radius="md" src="/app-icon.png" />
               <div className="mt-1">AK777</div>
               <div className="text-[11px]">Wining Grand</div>
-              <div className="text-white bg-[#cb352d] rounded-md w-full py-[4px] text-center mt-1 text-[12px]">Download</div>
+              <div onClick={ downloadApk } className="active:opacity-70 text-white bg-[#cb352d] rounded-md w-full py-[4px] text-center mt-1 text-[12px]">Download</div>
             </div>
           </Transition>
         </div>
-        {/* <button className="mt-[100px]" onClick={() => setshowDownload((isShowing) => !isShowing)}>
-          Toggle
-        </button> */}
       </div>
     </div>
   );
